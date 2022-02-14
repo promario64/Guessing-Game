8a7c3c3544a65c084fc94b3296c163488285e55e
promario64 <promario64@gmail.com>
2022-02-14 14:04:46 -0700

    Project is Done :)
    no help from online trying to make a guessing game with working lives

--- First  Program.py
+++ First  Program.py
@@ -1,27 +1,43 @@
 import random
+from tracemalloc import stop
 
 
 input("Are you ready to play? 'Y' or 'N' ")
 
 if "Y" or "Yes":
-    Value = int(random.randrange(1,11))
+    Value = int(random.randrange(1,100))
     Lives = int(5)
-    Hidden = 1
     print("you have 5 lives Good Luck :)")
-
-    print(Value)
+    print("The numbers are between 1 and 100")
 
     print("")
     Guess = int(input("What is your guess? "))
    
+    #loops untill lives are done
+    while Lives >= int(1):
+        if Guess == Value: 
+            print("You Win :)")
+            exit()
+        if Guess < Value:
+            print("Your Guess is higher then the number ")
+            
+            Lives -= 1
+            print("You Now have " + str(Lives) + " Lives left" )
+            print("")
+            Guess = int(input("What is your guess? "))
+        if Guess > Value:
+            print("Your guess is lower then the Number")
 
-    if Guess == Value: 
-        print("You Win :)")
-        exit()
-
-
-
-
+            Lives -= 1
+            print("You Now have " + str(Lives) + " Lives left" )
+            print("")
+            Guess = int(input("What is your guess? "))
+        
+    if Lives == int(0):
+        print("Sorry you lose :(")
 
 else:
     exit()
+
+# Didn't Look online to figure it out 1.5 days it took
+
